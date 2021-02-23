291a40c3e40f09b98aeda40297aad3d17dd6d5cb
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-02-22 23:51:35 -0600

    Hw1 (#2)
    * complete hw1 demo
    
    * Add site header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  chakra,
+  Box,
+  Flex,
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 import Basic from "./components/Basic";
 import ReactThreeFiber from "./components/ReactThreeFiber";
@@ -9,6 +19,36 @@ function App() {
   return (
     <Box height="100vh" maxWidth="100vw">
       <Router>
+        <chakra.header
+          pos="fixed"
+          top="0"
+          bg="white"
+          zIndex="3"
+          left="0"
+          right="0"
+          width="full"
+        >
+          <chakra.div height="4.5rem" mx="auto" maxW="1200px">
+            <Flex w="100%" h="100%" px="6" align="center" justify="flex-end">
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  aria-label="Options"
+                  icon={<HamburgerIcon />}
+                  variant="outline"
+                />
+                <MenuList>
+                  <MenuItem as={Link} to="/">
+                    ThreeJS Hello World
+                  </MenuItem>
+                  <MenuItem as={Link} to="/rtf">
+                    HW #1 - Demo
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+          </chakra.div>
+        </chakra.header>
         <Switch>
           <Route path="/rtf">
             <ReactThreeFiber />
--- src/components/ReactThreeFiber.tsx
+++ src/components/ReactThreeFiber.tsx
@@ -2,15 +2,12 @@ import React, { useRef, useState } from "react";
 import { Canvas, MeshProps, useFrame } from "react-three-fiber";
 import type { Mesh } from "three";
 
-const Box: React.FC<MeshProps> = (props) => {
-  // This reference will give us direct access to the mesh
+const Sphere: React.FC<MeshProps> = (props) => {
   const mesh = useRef<Mesh>();
 
-  // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
-  // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
     if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
@@ -20,11 +17,36 @@ const Box: React.FC<MeshProps> = (props) => {
       {...props}
       ref={mesh}
       scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
     >
-      <boxBufferGeometry args={[1, 1, 1]} />
+      <sphereBufferGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+const Cylinder: React.FC<MeshProps> = (props) => {
+  const mesh = useRef<Mesh>();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <cylinderBufferGeometry args={[1, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   );
@@ -35,8 +57,8 @@ const Demo = () => {
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Sphere position={[-2.2, 0, 0]} />
+      <Cylinder position={[2.2, 0, 0]} />
     </Canvas>
   );
 };
